0152e8d55e7ea5b035293247f9468279677b5a52
SantanuAdhikary <santanuadhikary13@gmail.com>
2026-04-04 18:59:51 +0530

    querySelector added

---
+++ JAVASCRIPT/DOM/target.js
@@ -0,0 +1,26 @@
+
+
+let nav = document.getElementsByTagName("nav")
+console.log(nav[0])
+
+
+let nav1 = document.querySelector("nav")
+console.log(nav1)
+
+let para = document.querySelector("#para");
+console.log(para)
+
+let login = document.querySelector(".login")
+console.log(login)
+
+
+let li = document.querySelector("li")
+
+console.log(li)
+
+let card = document.querySelector(".card")
+console.log(card)
+
+
+let cards = document.querySelectorAll(".card")
+console.log(cards)
