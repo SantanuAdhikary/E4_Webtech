0e703549c22df18e3149f3cb2871632fc109fec4
SantanuAdhikary <santanuadhikary13@gmail.com>
2026-04-09 19:08:19 +0530

    background task by js

---
+++ JAVASCRIPT/backgroundTask/app.js
@@ -0,0 +1,27 @@
+
+
+let convertRed =()=>{
+
+    let outer = document.querySelector(".outer")
+    outer.style.backgroundColor = "red"
+}
+let convertGreen =()=>{
+
+    let outer = document.querySelector(".outer")
+    outer.style.backgroundColor = "green"
+}
+let convertBlue =()=>{
+
+    let outer = document.querySelector(".outer")
+    outer.style.backgroundColor = "blue"
+}
+let convertBlack =()=>{
+
+    let outer = document.querySelector(".outer")
+    outer.style.backgroundColor = "black"
+}
+let convertWhite =()=>{
+
+    let outer = document.querySelector(".outer")
+    outer.style.backgroundColor = "white"
+}
