1f2631d84cd627074bed1aa14d91bc023c69c98c
SantanuAdhikary <santanuadhikary13@gmail.com>
2026-04-18 19:04:14 +0530

    code done

--- JAVASCRIPT/productApp/home.js
+++ JAVASCRIPT/productApp/home.js
@@ -13,3 +13,29 @@ logout.addEventListener("click",()=>{
 
     open("./signup.html")
 })
+
+
+let main = document.querySelector("main")
+let getData =async()=>{
+    try{
+          let res = await fetch("https://fakestoreapi.com/products");
+          let data = await res.json();
+          data.map((ele)=>{     
+                let div = document.createElement("div")
+
+                div.innerHTML = `<img src=${ele.image}> 
+                                <h3>${ele.title}</h3>
+                                <h3>Price : ${ele.price} $</h3>
+                              <h3> Ratings : ${ele.rating.rate}</h3> `
+
+                div.classList.add("card");
+                main.append(div)
+          })
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+}
+
+getData();  
